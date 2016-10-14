782b36c4751ce1a389f5caf1686dfc2b092a7e55
Alex Joubert <alex.joubert93@gmail.com>
2016-10-14 13:25:14 -0600

    realized the player class wasn't added in previous commits (oops)

---
+++ src/main/java/com/adaptionsoft/games/uglytrivia/Player.java
@@ -0,0 +1,18 @@
+package com.adaptionsoft.games.uglytrivia;
+
+public class Player {
+	int place;
+	String name;
+	
+	public Player(String name) {
+		super();
+		this.name = name;
+		this.place = 0;
+	}
+	
+	public void moveTo(int newPlace) {
+		this.place = newPlace;
+	}
+	
+	public int place() { return this.place; }
+}
